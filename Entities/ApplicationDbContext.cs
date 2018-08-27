e00ff29124b3837ccb86cad34c7b7687a9cb8d43
Ícaro Pinheiro Rodrigues <icaro.rodrigues@intranet.fip-moc.edu.br>
2018-08-27 14:26:13 -0300

    Migration Identity

--- Entities/ApplicationDbContext.cs
+++ Entities/ApplicationDbContext.cs
@@ -11,43 +11,45 @@ namespace webapi.Entities
 {
     public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
-        
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
-        
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             // Shorten key length for Identity
-			builder.Entity<IdentityUser>(entity => {
-				entity.Property(m => m.Email).HasMaxLength(127);
-				entity.Property(m => m.NormalizedEmail).HasMaxLength(127);
-				entity.Property(m => m.NormalizedUserName).HasMaxLength(127);
-				entity.Property(m => m.UserName).HasMaxLength(127);
-			});
-			builder.Entity<IdentityRole>(entity => {
-				entity.Property(m => m.Name).HasMaxLength(127);
-				entity.Property(m => m.NormalizedName).HasMaxLength(127);
-			});
-			builder.Entity<IdentityUserLogin<string>>(entity =>
-			{
-				entity.Property(m => m.LoginProvider).HasMaxLength(127);
-				entity.Property(m => m.ProviderKey).HasMaxLength(127);
-			});
-			builder.Entity<IdentityUserRole<string>>(entity =>
-			{
-				entity.Property(m => m.UserId).HasMaxLength(127);
-				entity.Property(m => m.RoleId).HasMaxLength(127);
-			});
-			builder.Entity<IdentityUserToken<string>>(entity =>
-			{
-				entity.Property(m => m.UserId).HasMaxLength(127);
-				entity.Property(m => m.LoginProvider).HasMaxLength(127);
-				entity.Property(m => m.Name).HasMaxLength(127);
+            builder.Entity<IdentityUser>(entity =>
+            {
+                entity.Property(m => m.Email).HasMaxLength(127);
+                entity.Property(m => m.NormalizedEmail).HasMaxLength(127);
+                entity.Property(m => m.NormalizedUserName).HasMaxLength(127);
+                entity.Property(m => m.UserName).HasMaxLength(127);
+            });
+            builder.Entity<IdentityRole>(entity =>
+            {
+                entity.Property(m => m.Name).HasMaxLength(127);
+                entity.Property(m => m.NormalizedName).HasMaxLength(127);
+            });
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.Property(m => m.LoginProvider).HasMaxLength(127);
+                entity.Property(m => m.ProviderKey).HasMaxLength(127);
+            });
+            builder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.Property(m => m.UserId).HasMaxLength(127);
+                entity.Property(m => m.RoleId).HasMaxLength(127);
+            });
+            builder.Entity<IdentityUserToken<string>>(entity =>
+            {
+                entity.Property(m => m.UserId).HasMaxLength(127);
+                entity.Property(m => m.LoginProvider).HasMaxLength(127);
+                entity.Property(m => m.Name).HasMaxLength(127);
 
-			});
+            });
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
