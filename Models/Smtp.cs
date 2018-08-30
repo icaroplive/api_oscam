33357c597bfb2731eb4f9fb1fbd32fc6f93a378e
Ícaro Pinheiro Rodrigues <icaro.rodrigues@intranet.fip-moc.edu.br>
2018-08-30 10:40:56 -0300

    Model CRUD Servidores SMTP

--- Models/Smtp.cs
+++ Models/Smtp.cs
@@ -9,9 +9,12 @@ namespace webapi.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
         public Guid id { get; set; }
+        [MaxLength(100)]
         public string endereco { get; set; }
         public int porta { get; set; }
+        [MaxLength(100)]
         public string sslTls { get; set; }
+        [MaxLength(100)]
         public string descricao { get; set; }
     }
 }
