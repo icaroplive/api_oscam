56038254fbdaaa4d93ed4ad76f8e3c1e5713a04a
Ícaro Pinheiro Rodrigues <icaro.rodrigues@intranet.fip-moc.edu.br>
2018-08-30 10:41:44 -0300

    Envia email

--- Models/SendMail.cs
+++ Models/SendMail.cs
@@ -7,21 +7,26 @@ namespace webapi.Models
 {
     public class SendMail
     {
-        public static SmtpResponse Send()
+        public static SmtpResponse Send(ConfEmailViewModel conf)
         {
             
             var smtpClient = new SmtpClient
             {
-                Host = "smtp.gmail.com", // set your SMTP server name here
-                Port = 587, // Port 
+                Host = conf.smtp.endereco, // set your SMTP server name here
+                Port = conf.smtp.porta, // Port 
                 EnableSsl = true,
-                Credentials = new NetworkCredential("icaroplive@gmail.comm", "@Aa102030")
+                Credentials = new NetworkCredential(conf.revendedor.emailSmtp, conf.revendedor.senha)
             };
-
-            using (var message = new MailMessage("icaroplive@gmail.com", "icaropinheiro@live.com")
+            var corpo = conf.ModeloEmail.corpo.Replace("[nome]", conf.cliente.nome);
+            corpo = corpo.Replace("[usuario]",conf.cliente.login);
+            corpo = corpo.Replace("[senha]",conf.cliente.senha);
+            corpo = corpo.Replace("[dataVencimento]", conf.Financeiro.dataVencimento.ToString("dd/MM/yy hh:mm"));
+            corpo = corpo.Replace("[servidor]",conf.servidor.urlCam);
+            using (var message = new MailMessage(conf.revendedor.emailSmtp, conf.cliente.email)
             {
-                Subject = "Subject",
-                Body = "Body"
+                IsBodyHtml = true,
+                Subject = conf.ModeloEmail.titulo,
+                Body = corpo
             })
             {
             try {
