e741c0cebb8754cc99e42171fd4d0de08b2b2e02
Ícaro Pinheiro Rodrigues <icaro.rodrigues@intranet.fip-moc.edu.br>
2018-08-30 11:15:05 -0300

    Corrigindo variavel da url do servidor CAM

--- Models/Oscam.cs
+++ Models/Oscam.cs
@@ -9,26 +9,25 @@ namespace webapi.Models
 {
     public class Oscam
     {
-        static string url = "http://ipr.net.br:8888/";
         private static readonly HttpClient _httpClient = new HttpClient();
         public async static Task<int> criarUsuarioAsync(string usuario, string senha, string nome,Servidor servidor,int disabled=0)
         {
             var credCache = new CredentialCache();
-            credCache.Add(new Uri(url), "Digest", new NetworkCredential(servidor.userCam, servidor.senhaCam));
+            credCache.Add(new Uri(servidor.servidorCam), "Digest", new NetworkCredential(servidor.userCam, servidor.senhaCam));
             var HttpHandler = new HttpClientHandler();
-            HttpHandler.Credentials = credCache.GetCredential(new Uri(servidor.urlCam), "Digest");
+            HttpHandler.Credentials = credCache.GetCredential(new Uri(servidor.servidorCam), "Digest");
             var httpClient = new HttpClient(HttpHandler);
-            var answer = httpClient.GetAsync(new Uri(String.Format("{4}/user_edit.html?user={0}&pwd={1}&description={2}&disabled={3}&group=1&action=Save",usuario,senha,nome,disabled,servidor.urlCam))).Result;
+            var answer = httpClient.GetAsync(new Uri(String.Format("{4}/user_edit.html?user={0}&pwd={1}&description={2}&disabled={3}&group=1&action=Save",usuario,senha,nome,disabled,servidor.servidorCam))).Result;
             return (int)answer.StatusCode;
         }
         public async static Task<int> deleteAsync(string usuario,Servidor servidor)
         {
             var credCache = new CredentialCache();
-            credCache.Add(new Uri(url), "Digest", new NetworkCredential("ew", "ew"));
+            credCache.Add(new Uri(servidor.servidorCam), "Digest", new NetworkCredential(servidor.userCam, servidor.senhaCam));
             var HttpHandler = new HttpClientHandler();
-            HttpHandler.Credentials = credCache.GetCredential(new Uri(servidor.urlCam), "Digest");
+            HttpHandler.Credentials = credCache.GetCredential(new Uri(servidor.servidorCam), "Digest");
             var httpClient = new HttpClient(HttpHandler);
-            var answer = httpClient.GetAsync(new Uri(String.Format("{0}/oscamapi.json?part=userstats&user={1}&action=delete",servidor.urlCam,usuario))).Result;
+            var answer = httpClient.GetAsync(new Uri(String.Format("{0}/oscamapi.json?part=userstats&user={1}&action=delete",servidor.servidorCam,usuario))).Result;
             
             
             return (int)answer.StatusCode;
