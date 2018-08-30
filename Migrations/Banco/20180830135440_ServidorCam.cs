7c05ccd97743509ab34d083b207f3869162dd91c
Ícaro Pinheiro Rodrigues <icaro.rodrigues@intranet.fip-moc.edu.br>
2018-08-30 17:05:09 -0300

    migration

---
+++ Migrations/Banco/20180830135440_ServidorCam.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace webapi.Migrations.Banco
+{
+    public partial class ServidorCam : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "senhaCam",
+                table: "Servidor",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "servidorCam",
+                table: "Servidor",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "userCam",
+                table: "Servidor",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "senhaCam",
+                table: "Servidor");
+
+            migrationBuilder.DropColumn(
+                name: "servidorCam",
+                table: "Servidor");
+
+            migrationBuilder.DropColumn(
+                name: "userCam",
+                table: "Servidor");
+        }
+    }
+}
