92cdbdcef38fba0e2043c324371c0b65d061800f
Ícaro Pinheiro Rodrigues <icaro.rodrigues@intranet.fip-moc.edu.br>
2019-03-08 10:43:17 -0300

    ClienteRevendedorViewModel

---
+++ Models/ClienteRevendedorViewModel.cs
@@ -0,0 +1,8 @@
+namespace webapi.Models
+{
+    public class ClienteRevendedorViewModel
+    {
+        public Cliente Cliente { get; set; }
+        public Revendedor Revendedor { get; set; }
+    }
+}
