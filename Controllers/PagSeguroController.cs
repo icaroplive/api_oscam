ea748a09d20abbda6c3279756f20036ce4cbc33d
Icaro Pinheiro <icaropinheiro@live.com>
2021-03-29 17:25:51 -0300

    Update gitignore

--- Controllers/PagSeguroController.cs
+++ Controllers/PagSeguroController.cs
@@ -8,7 +8,7 @@ namespace webapi.Controllers
         RestClient client = new RestClient();
         public CieloController() {
             client = new RestClient("https://ws.pagseguro.uol.com.br/");
-            request = new RestRequest("v2/checkout/?email=icaropinheiro@live.com&token=A9D849A6ABA64541B7EF2C665B57E0A5", Method.POST);
+            request = new RestRequest("v2/checkout/?email=email&token=token", Method.POST);
             
             
         }
